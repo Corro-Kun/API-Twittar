d4fe9eb046796e37692ffe35de828d67a045ae6b
Corro-Kun <108897765+Corro-Kun@users.noreply.github.com>
2023-04-15 20:03:54 -0500

    Update app.js
    añadir diferencias en las rutas

--- src/app.js
+++ src/app.js
@@ -9,5 +9,11 @@ App.use(express.json())
 App.use(cors())
 App.use(morgan("dev"))
 
+App.use((req, res, next) =>{
+  res.status(404).json({
+     Error: "no existe la ruta solicitada"
+  })
+})
+
 
 export default App
