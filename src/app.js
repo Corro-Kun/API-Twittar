35f0966327d1edc2f177ae4c69c1298173a6402a
Corro-Kun <108897765+Corro-Kun@users.noreply.github.com>
2023-04-23 19:58:26 -0500

    arregle el bug de la api

--- src/app.js
+++ src/app.js
@@ -9,11 +9,4 @@ App.use(express.json())
 App.use(cors())
 App.use(morgan("dev"))
 
-App.use((req, res, next) =>{
-  res.status(404).json({
-     Error: "no existe la ruta solicitada"
-  })
-})
-
-
 export default App
