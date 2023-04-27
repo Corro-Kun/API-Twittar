2187fdfbc5c5268922f7d58d60b723e1fb8c8219
Corro-Kun <108897765+Corro-Kun@users.noreply.github.com>
2023-04-27 18:08:10 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,4 +9,10 @@ App.use(express.json())
 App.use(cors())
 App.use(morgan("dev"))
 
+App.use((req, res, next) =>{
+  res.status(404).json({
+     Error: "no existe la ruta solicitada"
+  })
+})
+
 export default App
